266424347fe292d5d2e9319beaaccf5c43a8e862
yarelbv <yarel21@outlook.es>
2022-03-28 15:24:37 -0600

    Integración de soluciones

---
+++ calculadora-pojo/calculadora.java
@@ -0,0 +1,33 @@
+public class calculadora {
+
+public int suma (int a, int b){
+
+return a+b;
+
+}
+
+
+
+public int resta (int a, int b){
+
+return a-b;
+
+}
+
+
+
+public int multiplica (int a, int b){
+
+return a*b;
+
+}
+
+
+
+public int divide (int a, int b){
+
+return a/b;
+
+}
+
+}
